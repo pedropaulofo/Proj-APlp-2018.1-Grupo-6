2796bb1324bdf1918b8af56f2ee5b75d309bc992
Pedro Paulo <pedro.oliveira@ccc.ufcg.edu.br>
2019-04-18 02:17:12 -0300

    Movimentos do bispo funcionando. Ajustes gerais em Pawn e na exibição da tela.

---
+++ cpp/ProjPLP-NaraShogi C++/bishop.cpp
@@ -0,0 +1,30 @@
+#include "pch.h"
+#include <stdio.h>
+
+//Bispo : Move-se apenas para as diagonais.
+
+bool is_bishop_move(board_pos origin, board_pos target, char players_map[BOARDSIZE][BOARDSIZE]) {
+
+	char identifier = players_map[origin.line_pos][origin.column_pos];
+
+	int i = origin.column_pos; 
+	int j = origin.line_pos;   
+	int k = target.column_pos; 
+	int l = target.line_pos; 
+
+	while(i != k){
+		if (i < k) i++;
+		if (i > k) i--;
+		if (j < l) j++;
+		if (j > l) j--;
+
+		if (players_map[j][i] == identifier) {
+			return false; // FOUND OWN PLAYER'S PIECE ON THE WAY
+		}
+		else if (i != k && players_map[j][i] != EMPTYCELL) {
+			return false; // OPONENT PIECE FOUND HALFWAY, INVALID MOVE,
+		}
+	}
+	return j == target.line_pos; // TRUE if for X moves towards targeted LINE, it has moved X COLUMNS in the same direction 
+}
+
--- cpp/ProjPLP-NaraShogi C++/pawn.cpp
+++ cpp/ProjPLP-NaraShogi C++/pawn.cpp
@@ -3,24 +3,13 @@
 
 //Peão : Move-se apenas uma casa para frente.
 
-bool is_pawn_move(board_pos origin, board_pos target, char playerboard[BOARDSIZE][BOARDSIZE]) {
-	//player true = player2
-	if (playerboard[target.line_pos][target.column_pos] == playerboard[origin.line_pos][origin.column_pos]) {
-		printf("Nao pode comer peca propria");
+bool is_pawn_move(board_pos origin, board_pos target, bool is_player1) {
+
+	if (is_player1 && (origin.column_pos != target.column_pos || origin.line_pos - 1 != target.line_pos)) {
 		return false;
 	}
-	if (playerboard[origin.line_pos][origin.column_pos] == '1') {
-		if (origin.column_pos != target.column_pos || origin.line_pos - 1 != target.line_pos) {
-			printf("invalido jog 1");
-			return false;
-		}
-	}
-
-	if (playerboard[origin.line_pos][origin.column_pos] == '2') {
-		if (origin.column_pos != target.column_pos || origin.line_pos + 1 != target.line_pos) {
-			printf("invalido jog 2");
-			return false;
-		}
+	else if (!is_player1 && (origin.column_pos != target.column_pos || origin.line_pos + 1 != target.line_pos)) {
+		return false;
 	}
 	return true;
 }
