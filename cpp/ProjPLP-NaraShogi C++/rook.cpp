d77665993dabca37655fdf3a6ba33dacafb79536
Aislan Jefferson <aislanjsb@gmail.com>
2019-04-25 16:00:15 -0300

    Adicionado funcionalidade do rook

--- cpp/ProjPLP-NaraShogi C++/rook.cpp
+++ cpp/ProjPLP-NaraShogi C++/rook.cpp
@@ -10,9 +10,42 @@ bool is_rook_move(board_pos origin, board_pos target, char players_map[BOARDSIZE
 	int column_iterator = origin.column_pos;
 	int tar_l = target.line_pos;
 	int tar_c = target.column_pos;
-
+	if(target.line_pos!=origin.line_pos && target.column_pos!=origin.column_pos){
+		return false;
+	}
+		if(target.line_pos!=origin.line_pos){
+			if(target.line_pos<origin.line_pos){
+			for(int i = origin.line_pos-1; i > target.line_pos; i--){
+				if(players_map[i][origin.column_pos] != NOPLAYER){
+					return false;
+				}
+			}
+			}else{
+				for(int i = origin.line_pos+1; i < target.line_pos; i++){
+					if(players_map[i][origin.column_pos] != NOPLAYER){
+						return false;
+					}
+				}
+			}
+			return true;
+			}
+			if(target.column_pos!=origin.column_pos){
+			if(target.column_pos<origin.column_pos){
+			for(int i = origin.column_pos-1; i > target.column_pos; i--){
+				if(players_map[origin.line_pos][i] != NOPLAYER){
+					return false;
+				}
+			}
+			}else{
+				for(int i = origin.column_pos+1; i < target.column_pos; i++){
+					if(players_map[origin.line_pos][i] != NOPLAYER){
+						return false;
+					}
+				}
+			}
+			}
+		return true;
 	// FALTA TRATAR AQUI SE HA PECAS NO CAMINHO  
-
 	return false;
 }
 
